7f98a1fc1ece91c509b89ea9c977d98b69bc8fa1
Panithee Virojwongchai <panithee.pnt@mail.kmutt.ac.th>
2022-09-01 20:47:59 +0700

    Finish fillstartword

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,20 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
-}
+
+    if (word == null || word == undefined) {
+      return undefined;
+    } else if (startWord.localeCompare(word)) {
+      return word;
+    } else {
+      return startWord + word;
+    }
+  }
+  console.log(fillStartWord("1-2565-", "Hello World"));
+  console.log(fillStartWord("JS", "beginner"));
+  console.log(fillStartWord("first", "firststep"));
+  console.log(fillStartWord("first", null));
+  console.log(fillStartWord("first", undefined));
+  
+
 module.exports = fillStartWord
