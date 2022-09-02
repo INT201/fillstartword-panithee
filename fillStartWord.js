dcec2c024b41f41a7eea47a319f842601a1abcc7
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-09-02 14:39:08 +0700

    Finished fillStartWord2

--- fillStartWord.js
+++ fillStartWord.js
@@ -5,17 +5,17 @@ function fillStartWord(startWord, word) {
 
     if (word == null || word == undefined) {
       return undefined;
-    } else if (startWord.localeCompare(word)) {
+    } else if (startWord===word.slice(0, startWord)) {
       return word;
     } else {
       return startWord + word;
     }
   }
-  console.log(fillStartWord("1-2565-", "Hello World"));
-  console.log(fillStartWord("JS", "beginner"));
-  console.log(fillStartWord("first", "firststep"));
-  console.log(fillStartWord("first", null));
-  console.log(fillStartWord("first", undefined));
+  // console.log(fillStartWord("1-2565-", "Hello World"));
+  // console.log(fillStartWord("JS", "beginner"));
+  // console.log(fillStartWord("first", "firststep"));
+  // console.log(fillStartWord("first", null));
+  // console.log(fillStartWord("first", undefined));
   
 
 module.exports = fillStartWord
