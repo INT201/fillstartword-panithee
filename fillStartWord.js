750e813cc4be558bdc5714bb2762f54559d477fc
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-09-11 22:29:16 +0700

    Finished again

--- fillStartWord.js
+++ fillStartWord.js
@@ -5,17 +5,13 @@ function fillStartWord(startWord, word) {
 
     if (word == null || word == undefined) {
       return undefined;
-    } else if (startWord===word.slice(0, startWord.length)) {
+    } else if (word.includes(startWord)) {
       return word;
     } else {
       return startWord + word;
     }
   }
-  // console.log(fillStartWord("1-2565-", "Hello World"));
-  // console.log(fillStartWord("JS", "beginner"));
-  // console.log(fillStartWord("first", "firststep"));
-  // console.log(fillStartWord("first", null));
-  // console.log(fillStartWord("first", undefined));
+
   
 
 module.exports = fillStartWord
